fd9b633ff80c1a66617dfe847906a908a8d0508b
emresvd <emresevindi99@gmail.com>
2022-12-28 22:50:57 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -8,25 +8,26 @@ repo_url = sys.argv[-1]
 r = requests.get(repo_url)
 soup = BeautifulSoup(r.content, 'html.parser')
 
-# for i in soup.find_all('a'):
-#     if "blob/master" in i.get('href'):
-#         url = (
-#             urljoin(repo_url, i.get('href'))
-#             .replace("github.com", "raw.githubusercontent.com")
-#             .replace("blob/", "")
-#         )
-#         print(url)
+tree_urls = [repo_url]
 
-folder_urls = [repo_url]
-
-for i in folder_urls:
-    print("-", end="")
+for i in tree_urls:
     r = requests.get(i)
     soup = BeautifulSoup(r.content, 'html.parser')
     for j in soup.find_all('a'):
         if "tree/master" in j.get('href'):
             url = urljoin(repo_url, j.get('href'))
-            if url not in folder_urls:
-                folder_urls.append(url)
+            if url not in tree_urls:
+                tree_urls.append(url)
 
-print(folder_urls)
+for tree_url in tree_urls:
+    r = requests.get(tree_url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    for i in soup.find_all('a'):
+        if "blob/master" in i.get('href'):
+            url = (
+                urljoin(repo_url, i.get('href'))
+                .replace("github.com", "raw.githubusercontent.com")
+                .replace("blob/", "")
+            )
+            print(url)
