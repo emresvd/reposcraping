970a94484217e1c27c77b50d44933fb6a9563556
emresvd <emresevindi99@gmail.com>
2023-01-01 22:10:30 +0300

    modified:   reposcraping/__init__.py

--- reposcraping/__init__.py
+++ reposcraping/__init__.py
@@ -1,6 +1,6 @@
-from bs4 import BeautifulSoup as _BeautifulSoup
-import requests as _requests
-from urllib.parse import urljoin as _urljoin
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urljoin
 
 
 class RepoScraping(object):
@@ -15,22 +15,22 @@ class RepoScraping(object):
 
     def __prepare_tree_urls(self) -> None:
         for i in self.tree_urls:
-            r = _requests.get(i)
-            soup = _BeautifulSoup(r.content, 'html.parser')
+            r = requests.get(i)
+            soup = BeautifulSoup(r.content, 'html.parser')
 
             for j in soup.find_all('a'):
                 if "tree/master" in j.get('href'):
-                    url = _urljoin(self.repo_url, j.get('href'))
+                    url = urljoin(self.repo_url, j.get('href'))
                     if url not in self.tree_urls:
                         self.tree_urls.append(url)
 
     def __prepare_file_urls(self) -> None:
         for tree_url in self.tree_urls:
-            r = _requests.get(tree_url)
-            soup = _BeautifulSoup(r.content, 'html.parser')
+            r = requests.get(tree_url)
+            soup = BeautifulSoup(r.content, 'html.parser')
 
             for i in soup.find_all('a'):
                 if "blob/master" in i.get('href'):
-                    url = _urljoin(self.repo_url, i.get('href'))
+                    url = urljoin(self.repo_url, i.get('href'))
                     if url not in self.file_urls:
                         self.file_urls.append(url)
