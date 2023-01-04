ae10d087ae70590b6827a04b493c8d7029603615
emresvd <emresevindi99@gmail.com>
2023-01-04 13:02:20 +0300

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with codecs.open("README.md", "r", "utf-8") as f:
 
 setup(
     name='reposcraping',
-    version='0.0.2',
+    version='1.0.0',
     description='Scraping GitHub repository',
     long_description=long_description,
     long_description_content_type='text/markdown',
