ccc01c63f1341bcfd46196940cac24b4e6f93480
emresvd <emresevindi99@gmail.com>
2023-01-04 12:17:17 +0300

    deleted:    Makefile	make.py

--- Makefile
+++
@@ -1,7 +0,0 @@
-all: build
-
-build:
-	venv/Scripts/python setup.py sdist bdist_wheel
-
-clear:
-	venv/Scripts/python setup.py clean --all
---
+++ make.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import shutil
+
+if sys.argv[-1] == "clean":
+    for i in ["build", "dist", "reposcraping.egg-info"]:
+        shutil.rmtree(i)
+
+if sys.argv[-1] == "build":
+    os.system("python setup.py sdist bdist_wheel")
