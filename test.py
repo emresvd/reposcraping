8ddd5a88a701f771db936de8b69233cf663a1531
emresvd <109899306+emresvd@users.noreply.github.com>
2023-03-06 14:34:22 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from reposcraping import RepoScraping
 from reposcraping.cloner import Cloner
+import os
 
 scraping = RepoScraping(
     "https://github.com/emresvd/random-video",
@@ -63,4 +64,94 @@ assert scraping.file_urls == [
     'https://github.com/emresvd/random-video/blob/master/video/migrations/__init__.py',
     'https://github.com/emresvd/random-video/blob/master/video/migrations/__pycache__/__init__.cpython-37.pyc'
 ]
-print("✓ file_urls test passed")
+print("✓ file_urls test passed\n")
+
+
+cloner = Cloner(scraping)
+cloner.clone(
+    paths={
+        ".py": "files/python_files",
+        ".txt": "files/text_files",
+        ".md": "files/markdown_files",
+        ".html": "files/html_files",
+        "": "files/other_files",
+    },
+    only_file_name=False,
+    p=True,
+)
+
+print("\nlooking for files")
+assert os.listdir("files") == [
+    'html_files',
+    'markdown_files',
+    'other_files',
+    'python_files',
+    'text_files'
+]
+print("✓ files found")
+
+print("\nlooking for html files")
+assert os.listdir("files/html_files") == [
+    'emresvd_random-video_templates_download.html',
+    'emresvd_random-video_templates_index.html'
+]
+print("✓ html files found")
+
+print("\nlooking for markdown files")
+assert os.listdir("files/markdown_files") == [
+    'emresvd_random-video_LICENSE.md',
+    'emresvd_random-video_README.md'
+]
+print("✓ markdown files found")
+
+print("\nlooking for other files")
+assert os.listdir("files/other_files") == [
+    'emresvd_random-video_.gitignore',
+    'emresvd_random-video_db.sqlite3',
+    'emresvd_random-video_random_video___pycache___settings.cpython-37.pyc',
+    'emresvd_random-video_random_video___pycache___urls.cpython-37.pyc',
+    'emresvd_random-video_random_video___pycache___wsgi.cpython-37.pyc',
+    'emresvd_random-video_random_video___pycache_____init__.cpython-37.pyc',
+    'emresvd_random-video_static_favicon.ico',
+    'emresvd_random-video_static_ic_launcher-playstore.png',
+    'emresvd_random-video_video_migrations___pycache_____init__.cpython-37.pyc',
+    'emresvd_random-video_video___pycache___admin.cpython-37.pyc',
+    'emresvd_random-video_video___pycache___models.cpython-37.pyc',
+    'emresvd_random-video_video___pycache___random_video.cpython-37.pyc',
+    'emresvd_random-video_video___pycache___views.cpython-37.pyc',
+    'emresvd_random-video_video___pycache_____init__.cpython-37.pyc'
+]
+print("✓ other files found")
+
+print("\nlooking for python files")
+assert os.listdir("files/python_files") == [
+    'emresvd_random-video_manage.py',
+    'emresvd_random-video_random_video_asgi.py',
+    'emresvd_random-video_random_video_settings.py',
+    'emresvd_random-video_random_video_urls.py',
+    'emresvd_random-video_random_video_wsgi.py',
+    'emresvd_random-video_random_video___init__.py',
+    'emresvd_random-video_video_admin.py',
+    'emresvd_random-video_video_apps.py',
+    'emresvd_random-video_video_migrations___init__.py',
+    'emresvd_random-video_video_models.py',
+    'emresvd_random-video_video_random_video.py',
+    'emresvd_random-video_video_tests.py',
+    'emresvd_random-video_video_views.py',
+    'emresvd_random-video_video___init__.py'
+]
+print("✓ python files found")
+
+print("\nlooking for text files")
+assert os.listdir("files/text_files") == [
+    'emresvd_random-video_requirements.txt',
+    'emresvd_random-video_special_search_car.txt',
+    'emresvd_random-video_special_search_food.txt',
+    'emresvd_random-video_special_search_rocket.txt',
+    'emresvd_random-video_special_search_space.txt',
+    'emresvd_random-video_special_search_travel.txt',
+    'emresvd_random-video_words.txt'
+]
+print("✓ text files found")
+
+print("\n✓ all tests passed")
